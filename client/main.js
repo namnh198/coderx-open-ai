a710707c2e86412f0eefaa203a65c6404bf70caa
namnh198 <namnhn98@gmail.com>
2023-02-01 13:18:42 +0700

    deployment

--- client/main.js
+++ client/main.js
@@ -59,7 +59,7 @@ const handleSubmit = async (e) => {
     chatContainer.scrollTop = chatContainer.scrollHeight;
     const messageDiv = document.getElementById(uniqueId);
     loader(messageDiv);
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://coderx-open-ai.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ prompt: data.get('prompt') })
